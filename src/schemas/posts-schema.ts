fa927ea6a2c70c188463288d34b9c283ba69da7d
RubsRafa <rubiarafaela2000@gmail.com>
2023-06-02 17:57:22 -0300

    feat: post schema allow null params

--- src/schemas/posts-schema.ts
+++ src/schemas/posts-schema.ts
@@ -6,7 +6,7 @@ const isVideo = /\.(mp4|avi|mov|wmv)$/i;
 
 export const postsSchema = Joi.object<PostParams>({
     type: Joi.number().required(),
-    video: Joi.string().regex(isVideo),
-    image: Joi.string().regex(isImage),
-    text: Joi.string(),
+    video: Joi.string().allow(null).regex(isVideo),
+    image: Joi.string().allow(null).regex(isImage),
+    text: Joi.string().allow(null),
 })
