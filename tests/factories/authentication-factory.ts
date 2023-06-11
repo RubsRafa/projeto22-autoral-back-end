f4057a676cffa6b1c9bb06cab16993e76328f18e
RubsRafa <rubiarafaela2000@gmail.com>
2023-06-11 15:02:41 -0300

    test: post /auth/signin integration test

--- tests/factories/authentication-factory.ts
+++ tests/factories/authentication-factory.ts
@@ -1,13 +1,18 @@
 import { prisma } from "@/config";
 import { faker } from "@faker-js/faker";
+import { Users } from "@prisma/client";
+import bcrypt from 'bcrypt';
 
 
-export async function createUser() {
+export async function createUser(user?: Partial<Users>) {
+    const incomingPassword = user?.password || faker.internet.password();
+    const hashedPassword = await bcrypt.hash(incomingPassword, 10);
+
     return await prisma.users.create({
         data: {
-            name: faker.word.words(),
-            email: faker.internet.email(),
-            password: faker.word.words(),
+            name: faker.internet.displayName(),
+            email: user?.email || faker.internet.email(),
+            password: hashedPassword,
             image: faker.image.avatar(),
         }
     })
@@ -15,9 +20,16 @@ export async function createUser() {
 
 export function returnUserBody(email?: string) {
     return {
-        name: faker.word.words(),
+        name: faker.internet.displayName(),
         email: email || faker.internet.email(),
-        password: faker.word.words(),
+        password: faker.internet.password(),
         image: faker.image.avatar(),
+    }
+}
+
+export function userSignIn(email: string, password: string) {
+    return {
+        email,
+        password,
     }
 }
--- tests/integration/authentication.test.ts
+++ tests/integration/authentication.test.ts
@@ -2,7 +2,8 @@ import httpStatus from 'http-status';
 import supertest from 'supertest';
 import app, { init } from '@/app';
 import { cleanDb } from '../helpers';
-import { createUser, returnUserBody } from '../factories';
+import { createUser, returnUserBody, userSignIn } from '../factories';
+import { faker } from '@faker-js/faker';
 
 
 beforeAll(async() => {
@@ -54,6 +55,53 @@ describe('POST /auth/signup', () => {
 
 })
 
-// describe('POST /auth/signin', () => {
-//     it('should respond with')
-// })
+describe('POST /auth/signin', () => {
+    it('should respond with status 400 when body is not given', async () => {
+        const response = await server.post('/auth/signin');
+    
+        expect(response.status).toBe(httpStatus.BAD_REQUEST);
+      });
+    
+    it('should respond with status 400 when body is not valid', async () => {
+        const user = {};
+    
+        const response = await server.post('/auth/signin').send(user);
+        expect(response.status).toBe(httpStatus.BAD_REQUEST);
+      });
+
+    it('should respond with status 404 if email does not exists', async () => {
+        const user = await createUser();
+        const fakerEmail = faker.internet.email();
+        const newUser = userSignIn(fakerEmail, user.password);
+
+        const response = await server.post('/auth/signin').send(newUser);
+        expect(response.status).toBe(httpStatus.NOT_FOUND)
+    })
+
+    it('should respond with status 404 if password is incorrect', async () => {
+        const user = await createUser();
+        const fakerPassword = faker.internet.password();
+        const newUser = userSignIn(user.email, fakerPassword);
+
+        const response = await server.post('/auth/signin').send(newUser);
+        expect(response.status).toBe(httpStatus.NOT_FOUND)
+    })
+    it('should respond with status 200 and user info if login was successfull', async () => {
+        const password = faker.internet.password();
+        const user = await createUser({ password });
+        const signIn = userSignIn(user.email, password);
+
+        const response = await server.post('/auth/signin').send(signIn);
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body).toEqual({
+            info: {
+                userId: user.id,
+                image: user.image,
+                email: user.email,
+                name: user.name,
+                birthday: user.birthday,
+            },
+            token: expect.any(String)
+        })
+    })
+})
