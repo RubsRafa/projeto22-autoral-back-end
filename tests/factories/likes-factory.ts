c9e55be32338287af4ba31c78c6f792582d4a0b1
RubsRafa <rubiarafaela2000@gmail.com>
2023-06-21 15:08:04 -0300

    test: unitary tests for likes service functions

--- tests/factories/likes-factory.ts
+++ tests/factories/likes-factory.ts
@@ -1,5 +1,6 @@
 import { Posts, Users } from '@prisma/client';
 import { prisma } from '@/config';
+import { faker } from '@faker-js/faker';
 
 export async function createLike(user: Users, post: Posts) {
   return await prisma.likes.create({
@@ -23,4 +24,29 @@ export async function findLikeByUserAndPost(user: Users, post: Posts) {
       postId: post.id,
     },
   });
+}
+
+export function returnLikeInfo(user: Users, post: Posts) {
+  return {
+    id: faker.number.int({ max: 50 }),
+    userId: user.id,
+    postId: post.id,
+    createdAt: faker.date.anytime(),
+    updatedAt: faker.date.anytime(),
+  }
+}
+
+export function returnAllLikes() {
+  return {
+    id: faker.number.int({ max: 50 }),
+    userId: faker.number.int({ max: 50}),
+    postId: faker.number.int({ max: 50}),
+    createdAt: faker.date.anytime(),
+    updatedAt: faker.date.anytime(),
+    Users: {
+      id: faker.number.int({ max: 50}),
+      name: faker.internet.userName(),
+      image: faker.internet.avatar(),
+    }
+  }
 }
